224ae9e7b1041af8cfc27d6ee166381d16e198f2
Ridwan <itsridOfficcial@gmail.com>
2024-04-25 12:07:16 +0530

    initial commit

---
+++ pages/Components/Body.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { useState } from 'react';
+import { toDoContext } from '../Context/context';
+import Input from './Input';
+import ToDo from './ToDo';
+import Message from './Message';
+const Body = () => {
+
+  let [myToDo, setMyToDo] = useState([]);
+
+  let getNewItem = (text, date) => {
+    const toDoItem = [
+      ...myToDo,
+      {name : text , date : date},
+    ]
+    console.log()
+    setMyToDo(toDoItem)
+  }
+
+  let deleteItem = (index) => {
+    const newToDoItems = [...myToDo]
+    newToDoItems.splice(index, 1)
+    setMyToDo(newToDoItems)
+    alert('Task Deleted Successfully')
+  }
+  let finishItem = (index) => {
+    const newToDoItems = [...myToDo]
+    newToDoItems.splice(index, 1)
+    setMyToDo(newToDoItems)
+    alert('Congratulation , Task Completed')
+  }
+  
+  return (
+    <toDoContext.Provider value={{myToDo , deleteItem , finishItem}}>
+      <div className='d-flex flex-column p-2'>
+        <h1 className='w-100 text-center'>To-Do-App</h1>
+        <Input getNewItem={getNewItem} />
+        {myToDo.length === 0 ? <Message/> : <ToDo/>}
+    </div>
+    </toDoContext.Provider>
+    
+  )
+}
+
+
+export default Body
---
+++ pages/Components/toDoListRender.jsx
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { toDoContext } from '../Context/context';
+
+const ToDoList = ({ name, date, index }) => {
+  const { deleteItem } = useContext(toDoContext);
+  const { finishItem } = useContext(toDoContext);
+  return (
+    <tr>
+      <th className='text-center' scope={index}>{ index +1}</th>
+      <td className='text-center'>{name}</td>
+      <td className='text-center'>{date}</td>
+      <td className='text-center'>
+        <button className="btn btn-danger btn-sm me-2 my-2" onClick={() => { deleteItem(index) }}	>Delete</button>
+        <button className="btn btn-success btn-sm" onClick={() => { finishItem(index) }}>Finish</button>
+      </td>
+    </tr>  
+  )
+}
+
+export default ToDoList
