c02263c2c6c99b5ff9fa6850f46235019e880e2c
Ridwan <itsridOfficcial@gmail.com>
2024-06-10 22:40:49 +0530

    new

--- Server/index.js
+++ Server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const cors = require('cors')
-const path = require("path");
 require('./connection/connection')
 const userApi = require('./routes/users')
 const taskApi = require('./routes/task')
@@ -18,10 +17,10 @@ app.use('/app', userApi)
 app.use('/app/task' , taskApi)
 
 
-app.get("/", (req, res) => {
-app.use(express.static(path.resolve(__dirname, "Front-End", "build")));
-res.sendFile(path.resolve(__dirname, "Front-End", "build", "index.html"));
-});
+app.get("/", (req, res) => { 
+  res.send("Hello World")
+}
+  
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`)
